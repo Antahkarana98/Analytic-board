8446c4545e11825db9608f3568010e4bd01dd801
Antahkarana98 <miloardila98@gmail.com>
2025-06-07 10:28:04 -0500

    feat(TaskList): Creacion del filtro de la tareas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,17 +1,27 @@
-import { useTasksStore } from "../store/tasksStore";
-import CardTask from "./CardTask";
-
+import { useTasksStore } from "@/store/tasksStore";
+import CardTask from "@/components/CardTask";
+import { useFilterTasks } from "@/hooks/useFilterTasks";
 
 const TaskList = () => {
 
   const { tasks } = useTasksStore()
+  const { filteredTasks, setFilter } = useFilterTasks(tasks)
 
   return (
     <>
-      <h2 className="text-2xl font-bold">Lista de tareas</h2>
+      <div className="flex justify-between items-center w-1/2 mx-auto">
+        <h2 className="text-2xl font-bold">Lista de tareas</h2>
+        <div>
+          <select name="" id="" onChange={(e) => setFilter(e.target.value)}>
+            <option value="">Todas las tareas</option>
+            <option value="completed">Tareas completadas</option>
+            <option value="pending">Tareas pendientes</option>
+          </select>
+        </div>
+      </div>
 
       <div>
-        {tasks.map((task) => (
+        {filteredTasks.map((task) => (
           <CardTask key={task.id} task={task} />
         ))}
       </div>
