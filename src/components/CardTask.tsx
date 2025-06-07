e0beb0b1a1cad21bd6ea80353b9eca82a03cd8bb
Antahkarana98 <miloardila98@gmail.com>
2025-06-07 10:23:44 -0500

    refactor(CardTask): refactor del codigo para quitarle responsabilidad al componente

--- src/components/CardTask.tsx
+++ src/components/CardTask.tsx
@@ -1,8 +1,28 @@
-import type { Task } from "../types"
+import { useTasksApi } from "@/hooks/useTaskApi"
+import { useTasksStore } from "@/store/tasksStore"
+import type { Task } from "@/types"
+import { useState } from "react"
 
 const CardTask = ({ task }: { task: Task }) => {
 
   const { taskName, category, startDate, endDate, isCompleted } = task
+
+  const [isCompletedTask, setIsCompletedTask] = useState(isCompleted)
+
+  const { updateTask, getTaskById, deleteTask } = useTasksStore()
+  const { updateTaskApi, deleteTaskApi } = useTasksApi()
+
+  const handleUpdateTask = async (task: Task) => {    
+    await updateTaskApi(task.id, { ...task, isCompleted: !task.isCompleted })    
+    setIsCompletedTask(!task.isCompleted)
+    getTaskById(task.id)
+    updateTask({ ...task, isCompleted: !task.isCompleted })
+  }
+
+  const handleDeleteTask = async (id: Task['id']) => {
+    await deleteTaskApi(id)
+    deleteTask(id)
+  }
 
   return (
     <>
@@ -12,11 +32,27 @@ const CardTask = ({ task }: { task: Task }) => {
           <p>{ category }</p>
           <p>{ startDate.toString() }</p>
           <p>{ endDate.toString() }</p>
-          <button type="button">
+          <button 
+            type="button" 
+            onClick={() => handleUpdateTask(task)}
+            >
             Completar
           </button>
-          { isCompleted ? <p className="text-green-500">Completada</p> : <p className="text-red-500">Pendiente</p> }
-          <button type="button">Eliminar</button>
+
+          <button 
+            type="button" 
+            onClick={() => getTaskById(task.id)}
+          >
+            Editar
+          </button>
+
+          { isCompletedTask ? <p className="text-green-500">Completada</p> : <p className="text-red-500">Pendiente</p> }
+          <button 
+            type="button" 
+            onClick={() => handleDeleteTask(task.id)}
+          >
+            Eliminar
+          </button>
         </div>
       </div>
     </>
