60514b6d9f20315c703a9a6a52afb28467da0ef9
Antahkarana98 <miloardila98@gmail.com>
2025-06-08 16:38:46 -0500

    feat: comnetario en el schema

--- src/schemas/zod-task-schema.ts
+++ src/schemas/zod-task-schema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 
+// Funcion para validar el objeto que llega de la api
 export const taskSchema = z.object({
   id: z.string().uuid(),
   taskName: z.string().min(1, 'La tarea es requerida'),
