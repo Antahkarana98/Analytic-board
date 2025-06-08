d91b820df3f3c13647100b5e2c393db4c67a7351
Antahkarana98 <miloardila98@gmail.com>
2025-06-08 16:37:53 -0500

    refactor: cambio de estilos y organizacion del codigo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 
-import { useTasksStore } from "@/store/tasksStore";
 import { useEffect } from "react";
+import { useTasksStore } from "@/store/tasksStore";
 import Form from "@/components/Form";
 import TaskList from "@/components/TaskList";
+import PieChart from "@/components/PieChart";
+import BarChart from "@/components/BarChart";
 
 const Home = () => {
 
   const { getAllTasks } = useTasksStore()
   
+  // Funcion para traer todos las tareas
   const getTasks = async () => {
     const url = 'http://localhost:3001/tasks'
     const response  = await fetch(url)
@@ -21,12 +24,33 @@ const Home = () => {
 
   return (
     <>
-      <h1 className="text-5xl font-bold text-center my-5">Bienvenido al To Do List de Camilo 📆</h1>
-      <section className="flex justify-center">
-        <Form />
-      </section>
+      <h1 className="text-5xl font-bold text-center my-5 bg-white p-2 w-1/4 mx-auto">App <span className="text-sky-300">To Do</span> List</h1>
+      
+      <section className="grid grid-cols-2 gap-5 w-3/4 h-full mx-auto">
+        <section className="flex justify-center items-center">
+          <Form />
+        </section>
 
-      <section>
+        <section className="grid gap-5">
+          
+          <div className="relative">
+            <div className="relative z-10 block w-full px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 border-2 border-gray-900 rounded-lg bg-sky-200">
+              <PieChart />
+            </div>
+            <span className="absolute bottom-0 right-0 w-full h-full -mb-1.5 -mr-1.5 bg-gray-900 rounded-lg"></span>
+          </div>
+
+          <div className="relative">
+            <div className="relative z-10 block w-full px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 border-2 border-gray-900 rounded-lg bg-sky-200">
+              <BarChart />
+            </div>
+            <span className="absolute bottom-0 right-0 w-full h-full -mb-1.5 -mr-1.5 bg-gray-900 rounded-lg"></span>
+          </div>
+        </section>
+      </section>
+      
+
+      <section className="mt-10 w-3/4 mx-auto">
         <TaskList />
       </section>
     </>
