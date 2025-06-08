a8944fe084b1b3583896e0751403d8292dae8956
Antahkarana98 <miloardila98@gmail.com>
2025-06-08 16:37:04 -0500

    reafctor: refactorizacion de las constantes del hook

--- src/hooks/useFilterTasks.ts
+++ src/hooks/useFilterTasks.ts
@@ -1,14 +1,12 @@
-import type { Task } from "@/types";
 import { useMemo, useState } from "react";
-
-const FILTERS = {
-  completed: 'completed',
-  pending: 'pending'
-}
+import type { Task } from "@/types";
+import { FILTERS } from "@/constants/filters"
 
 export const useFilterTasks = (tasks: Task[]) => {
   const [filter, setFilter] = useState('')
-
+  
+  // Funcion para filtrar solo las tareas o que estan completadas o que estan pendientes
+  // segun el select, el useMemo es para que no se generen rederizaciones inncesarias
   const filteredTasks = useMemo(() => {
     if(filter === FILTERS.completed) return tasks.filter((task) => task.isCompleted)
     if(filter === FILTERS.pending) return tasks.filter((task) => !task.isCompleted)
