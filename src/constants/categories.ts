5081b40043acf621caf946b1fd6704c79faef6eb
Antahkarana98 <miloardila98@gmail.com>
2025-06-08 16:36:29 -0500

    feat: creacion de constantes

---
+++ src/constants/categories.ts
@@ -0,0 +1,10 @@
+export const CATEGORIES: { value: string, label: string }[] = [
+  { value: 'Trabajo', label: 'Trabajo' },
+  { value: 'Personal', label: 'Personal' },
+  { value: 'Educación', label: 'Educación' },
+  { value: 'Meditación', label: 'Meditación' },
+  { value: 'Salud', label: 'Salud' },
+  { value: 'Deporte', label: 'Deporte' },
+  { value: 'Finanzas', label: 'Finanzas' },
+  { value: 'Otros', label: 'Otros' },
+]
